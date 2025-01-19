685d0ec2c5f678d88d0a397b316574fbbae7c35b
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2025-01-19 22:54:14 +0530

    Scratch commit

---
+++ IOTKit/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace IOTKit;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ IOTKit/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace IOTKit;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
